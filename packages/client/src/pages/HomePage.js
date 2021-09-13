a1e8a7f90d694c55438470c40525114c224eae2a
Nathan Holt <nateofakin@gmail.com>
2021-09-13 13:56:11 -0400

    Refactored code

--- packages/client/src/pages/HomePage.js
+++ packages/client/src/pages/HomePage.js
@@ -14,9 +14,6 @@ export default function HomePage(props) {
       history.push(`/user/${user}`)    }
   }, [])
 
-  const handleSignIn = () => {
-    history.push("/log")
-  }
   
   return ( 
     <div className='home'>
@@ -25,7 +22,7 @@ export default function HomePage(props) {
         <h3>This is <b>the</b> premier timeline app</h3>
         <p>There are other planning and day organization apps, google calendar, but what are doing differently is that we allow for multiple people to be tracked at once. This could be used by authors to keep track of continuity of all of the characters in their stories. It could also be used by managers to help team productivity in an agile workplace/remote workplace, especially when the team members are in different timezones.</p>
         <h3>What are you waiting for?</h3>
-        <Button variant="outline-info" onClick={() => handleSignIn()}>Sign In/Up</Button>
+        <Button variant="outline-info" onClick={() => history.push("/log")}>Sign In/Up</Button>
       </div>
       <div className="info">
         <h3>Any Questions?</h3>
