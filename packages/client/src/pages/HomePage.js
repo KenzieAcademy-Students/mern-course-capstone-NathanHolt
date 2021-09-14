98c97ffeb8867c67d49e2a335e1230d0f0b55b65
Nathan Holt <nateofakin@gmail.com>
2021-09-13 20:49:38 -0400

    Added more ssections and spacing to intro. Also adjusted colors and made the button bigger

--- packages/client/src/pages/HomePage.js
+++ packages/client/src/pages/HomePage.js
@@ -24,26 +24,36 @@ export default function HomePage(props) {
             <h3>This is <b>the</b> premier timeline app</h3>
             <p>There are other planning and day organization apps, google calendar, but what are doing differently is that we allow for multiple people to be tracked at once. This could be used by authors to keep track of continuity of all of the characters in their stories. It could also be used by managers to help team productivity in an agile workplace/remote workplace, especially when the team members are in different timezones.</p>
             <h3>What are you waiting for?</h3>
-            <Button variant="outline-primary" onClick={() => history.push("/log")}>Sign In/Up</Button>
+            <div className="d-grid gap-2">
+              <Button size="lg" variant="outline-primary" onClick={() => history.push("/log")}>Sign In/Up</Button>
+            </div>
           </div>
         </div>
-        <div className="info section">
+        <div className="info anti section">
           <div className='anti-twist'>
             <h3>Any Questions?</h3>
             <div className="feature">
               <img alt="Timeline"/>
-              <p>Look at this neat feature</p>
-            </div>
-            <div className="feature">
-              <p>Look at this neat feature</p>
-              <img alt="Compare"/>
-            </div>
-            <div className="feature">
-              <img alt="Add paths"/>
-              <p>Look at this neat feature</p>
-            </div>      
+              <p>I'm baby fashion axe coloring book mustache flannel bespoke kogi farm-to-table activated charcoal seitan fam biodiesel YOLO. Iceland wayfarers before they sold out unicorn leggings, farm-to-table la croix VHS. Skateboard iPhone twee, leggings offal sartorial actually VHS echo park cliche poke occupy ethical. Hexagon four loko la croix brunch pinterest af. Activated charcoal asymmetrical DIY post-ironic raclette, narwhal deep v. </p>
+            </div>    
           </div>
         </div>
+        <div className="info section">
+          <div className='twist'>
+            <div className="feature">
+              <p>Franzen readymade seitan, tbh swag snackwave ennui bicycle rights la croix microdosing +1 bespoke shabby chic banh mi. Blog four loko kickstarter freegan. Bitters mumblecore kogi synth, VHS wayfarers scenester taiyaki pabst tilde tousled humblebrag franzen austin. 3 wolf moon jianbing leggings, kickstarter tote bag hell of helvetica. Humblebrag knausgaard pabst selvage gluten-free, tousled unicorn.</p>
+              <img alt="Compare"/>
+            </div>    
+          </div>    
+        </div> 
+        <div className="info anti section">
+          <div className='anti-twist'>
+            <div className="feature">
+              <img alt="Neat"/>
+              <p> Narwhal cray blog gluten-free polaroid. Aesthetic listicle whatever tousled beard. Air plant craft beer waistcoat copper mug mlkshk ethical. Cardigan subway tile salvia literally, distillery godard squid cray. Biodiesel sartorial subway tile vice, etsy pitchfork seitan single-origin coffee coloring book disrupt kombucha YOLO roof party dreamcatcher locavore.</p>
+            </div>    
+          </div>    
+        </div> 
         <div className="about section">
           <div className='twist'>
             <h3>Our Team</h3>
@@ -63,7 +73,7 @@ export default function HomePage(props) {
             </ul>
           </div>
         </div>
-        <div className="anti-about section">
+        <div className="about anti section">
           <div className='anti-twist'>
             <ul>
               <li>
