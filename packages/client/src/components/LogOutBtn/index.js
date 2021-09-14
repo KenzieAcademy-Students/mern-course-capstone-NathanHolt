814b45b49fae9e53a7614cb631ea9f332673da1c
Nathan Holt <nateofakin@gmail.com>
2021-09-13 22:20:41 -0400

    Added bootstrap button

--- packages/client/src/components/LogOutBtn/index.js
+++ packages/client/src/components/LogOutBtn/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import { useHistory } from "react-router-dom";
 
 export default function LogOutBtn() {
+    const history = useHistory()
+
+    const handleClick = () => {
+        console.log("logout")
+    }
+    
     return (
         <div>
-            
+            <Button variant="outline-danger">Log Out</Button>
         </div>
     )
 }
