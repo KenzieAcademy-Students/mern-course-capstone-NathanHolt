28dc6cbfbdc73cb3e1060c0f3c35ae4e44339c61
Nathan Holt <nateofakin@gmail.com>
2021-09-15 10:41:54 -0400

    Made skeleton of how the page should look

--- packages/client/src/pages/StoryPage.js
+++ packages/client/src/pages/StoryPage.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function StoryPage() {
     return (
-        <div>
-            StoryPage 
+        <div className="story-page">
+            <div className="story-header">header</div>
+            <div className="story-body">
+                <div className="story-sidebar">sidebar to add/manipulate characters</div>
+                <div className="story-main">
+                    <div className="story-line">timeline</div>
+                    <div className="story-character"></div>
+                    <div className="story-character"></div>
+                </div>
+            </div>
         </div>
     )
 }
