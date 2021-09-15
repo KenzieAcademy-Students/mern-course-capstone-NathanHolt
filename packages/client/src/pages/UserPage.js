e4a5738bf31d41c9d928d43d5188c4e91bdb92bd
Nathan Holt <nateofakin@gmail.com>
2021-09-15 00:00:23 -0400

    Created basic userPage

--- packages/client/src/pages/UserPage.js
+++ packages/client/src/pages/UserPage.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import LogOutBtn from 'components/LogOutBtn'
 
 export default function UserPage() {
+  const [display, setDisplay] = useState ('story')
   
   return (
-    <div>
-      
+    <div className="user-page">
+        <div className="header">
+          <LogOutBtn />
+        </div>
+        <div className="body">
+
+          {display === 'story' && <div>
+            <h1>Stories</h1>
+            <div className="stories">
+              <div className="story"></div>
+              <div className="story"></div>
+              <div className="story"></div>
+              <div className="story"></div>
+              <div className="story"></div>
+              <div className="story"></div>
+            </div>
+          </div>}
+
+          {display === 'story-form' && <div>
+            <h1>Create a new Story</h1>
+            <div className="stories">
+              <div className="story"></div>
+            </div>
+          </div>}
+
+          {display === 'user' && <div>
+            <h1>Edit your profile</h1>
+            <div className="stories">
+              <div className="story"></div>
+            </div>
+          </div>}
+
+        </div>
     </div>
   )
 }
