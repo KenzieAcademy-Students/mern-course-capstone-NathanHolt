c9b3e15d59abed0928fd74ca356b093cb5c8b181
Nathan Holt <nateofakin@gmail.com>
2021-09-16 14:11:00 -0400

    Added styling to signUp

--- packages/client/src/pages/SignUp.js
+++ packages/client/src/pages/SignUp.js
@@ -27,51 +27,53 @@ export default function SignUp() {
     history.push(`/user/${username}`)
   }
   return (
-    <div>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size='lg' controlId='email'>
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            autoFocus
-            type='username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size='lg' controlId='email'>
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size='lg' controlId='password'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size='lg' controlId='password'>
-          <Form.Label>Retype Password</Form.Label>
-          <Form.Control
-            type='password'
-            value={password2}
-            onChange={(e) => setPassword2(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size='lg' type='submit' disabled={!validateForm()}>
-          Sign Up
-        </Button>
-        <div>
-          <Button size='small' onClick={() => history.push("/log")}>
-            Already have an account?
+    <div className='sign-up'>
+      <div className='form'>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group size='lg' controlId='email'>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              autoFocus
+              type='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size='lg' controlId='email'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              autoFocus
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size='lg' controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size='lg' controlId='password'>
+            <Form.Label>Retype Password</Form.Label>
+            <Form.Control
+              type='password'
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
+            />
+          </Form.Group>
+          <Button block size='lg' type='submit' disabled={!validateForm()}>
+            Sign Up
           </Button>
-        </div>
-      </Form>
+          <div>
+            <Button size='small' onClick={() => history.push("/log")}>
+              Already have an account?
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
