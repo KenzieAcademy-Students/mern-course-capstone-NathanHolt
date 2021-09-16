03a1bf98112042a4375a2cb1e10a77da6fa07296
Nathan Holt <nateofakin@gmail.com>
2021-09-16 10:40:34 -0400

    Added buttons to swap between components

--- packages/client/src/components/UserHeader/index.js
+++ packages/client/src/components/UserHeader/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import LogOutBtn from 'components/LogOutBtn'
+import Button from 'react-bootstrap/Button'
 import './UserHeader.css'
 
 export default function UserHeader(props) {
   const { displayer } = props
   
     return (
-        <div className="header"> 
-          <LogOutBtn />
+        <div className="header">
+          <div className='logo'>
+            logo
+          </div>
+          <div className="nav">
+            <Button onClick={() => displayer('story')}>Stories</Button> 
+            <Button onClick={() => displayer('story-form')}>Create new Story</Button> 
+            <Button onClick={() => displayer('user')}>Edit Account</Button> 
+          </div>
+          <div className="log-out">
+            <LogOutBtn />
+          </div>
         </div>
     )
 }
