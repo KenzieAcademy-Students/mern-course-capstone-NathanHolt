cfbbe25a9d44426644ffb2c9503daed074051144
anthony klein <netflixsucks179@gmail.com>
2021-09-20 13:53:22 -0700

    sidebar

--- packages/client/src/pages/StoryPage.js
+++ packages/client/src/pages/StoryPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import LogOutBtn from 'components/LogOutBtn'
-
+import Sidebar from 'components/StoryPageSideBar'
 export default function StoryPage() {
     return (
         <div className="story-page">
@@ -9,7 +9,7 @@ export default function StoryPage() {
                 <LogOutBtn />
             </div>
             <div className="story-body">
-                <div className="story-sidebar">sidebar to add/manipulate characters</div>
+                <div className="story-sidebar"><Sidebar></Sidebar></div>
                 <div className="story-main">
                     <div className="story-line">timeline</div>
                     <div className="story-character"></div>
