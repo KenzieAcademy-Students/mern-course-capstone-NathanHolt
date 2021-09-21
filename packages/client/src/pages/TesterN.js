fa2f5f89dfb50fd8f44f51a6d990b774f7f6aa78
Nathan Holt <nateofakin@gmail.com>
2021-09-20 20:09:01 -0400

    Cleared out my testing page

--- packages/client/src/pages/TesterN.js
+++ packages/client/src/pages/TesterN.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Character from 'components/Character'
 import LogOutBtn from 'components/LogOutBtn'
 
 
@@ -8,8 +7,7 @@ export default function TesterN() {
     return (
         <div className="test-n">
             <h1>Nathan's component testing stage</h1>
-            <Character test={5} />
-            <Character test={8} />
+
             <LogOutBtn />
         </div>
     )
