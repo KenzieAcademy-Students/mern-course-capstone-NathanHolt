5b4dd1dc99a76ad5fcfac0d8a6aa6411a44d7487
Nathan Holt <nateofakin@gmail.com>
2021-09-21 15:24:00 -0400

    Added framework for the rest of the functions

--- packages/client/src/hooks/useUser.js
+++ packages/client/src/hooks/useUser.js
@@ -8,7 +8,27 @@ UserContext.displayName = 'UserContext'
 
 const UserReducer = (state, action) => {
     switch (action.type) {
-        case 'SET_USER':
+        case 'ADD_CHARACTER':
+            return {
+                ...state,
+            }
+        case 'DELETE_CHARACTER':
+            return {
+                ...state,
+            }
+        case 'EDIT_CHARACTER':
+            return {
+                ...state,
+            }
+        case 'ADD_PATH':
+            return {
+                ...state,
+            }
+        case 'DELETE_PATH':
+            return {
+                ...state,
+            }
+        case 'EDIT_PATH':
             return {
                 ...state,
             }
@@ -20,10 +40,44 @@ const UserReducer = (state, action) => {
 export const UserProvider = (props) => {
     const [state, dispatch] = useReducer(UserReducer, initialState)
 
+    const addCharacter = (payload) => {
+        dispatch({ type: 'ADD_CHARACTER', payload: payload})
+    }
+
+    const deleteCharacter = (payload) => {
+        dispatch({ type: 'DELETE_CHARACTER', payload: payload})
+    }
+
+    const editCharacter = (payload) => {
+        dispatch({ type: 'EDIT_CHARACTER', payload: payload})
+    }
+
+    const addPath = (payload) => {
+        dispatch({ type: 'ADD_PATH', payload: payload})
+    }
+
+    const deletePath = (payload) => {
+        dispatch({ type: 'DELETE_PATH', payload: payload})
+    }
+
+    const editPath = (payload) => {
+        dispatch({ type: 'EDIT_PATH', payload: payload})
+    }
+
+    const saveProgress = () => {
+        console.log('save')
+    }
 
     const value = useMemo(
         () => ({
             ...state,
+            addCharacter,
+            deleteCharacter,
+            editCharacter,
+            addPath,
+            deletePath,
+            editPath,
+            saveProgress,
         }),
         [state]
     )
