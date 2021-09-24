79958398c0d65ce954010dcd8151b7b713d1f29c
Nicholas Lai <nicholaslai155@gmail.com>
2021-09-24 01:13:34 -0500

    added timestamps

--- packages/server/src/models/user.js
+++ packages/server/src/models/user.js
@@ -1,31 +1,34 @@
 import mongoose from 'mongoose'
 const { ObjectId } = mongoose.Schema.Types
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-
-  passwordHash: {
-    type: String,
-    required: true,
-  },
-
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-
-  storyboard: [
-    {
-      type: ObjectId,
-      ref: 'Story',
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
     },
-  ],
-})
+
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+
+    storyboard: [
+      {
+        type: ObjectId,
+        ref: 'Story',
+      },
+    ],
+  },
+  { timestamps: true }
+)
 
 const User = mongoose.model('User', userSchema)
 
