628e529c9efffd34a687ce76e34cdef2fa2e8423
Nathan Holt <nateofakin@gmail.com>
2021-09-27 10:56:32 -0400

    Got a few initial reducer cases set up and the comment scaffolding for the ones that aren't fully set up. All are currently untested however

--- packages/client/src/hooks/useUser.js
+++ packages/client/src/hooks/useUser.js
@@ -5,19 +5,19 @@ const initialState = {
     author: '',
     created: 0,
     characters: [
-        {
-            name: '',
-            description: '',
-            color: '',
-            paths: [
-                {
-                    name: '',
-                    description: '',
-                    start: 0,
-                    end: 0,
-                },
-            ]
-        },
+        // {
+        //     name: '',
+        //     description: '',
+        //     color: '',
+        //     paths: [
+        //         {
+        //             name: '',
+        //             description: '',
+        //             start: 0,
+        //             end: 0,
+        //         },
+        //     ]
+        // },
     ],
 }
 
@@ -25,37 +25,106 @@ export const UserContext = createContext(initialState)
 
 UserContext.displayName = 'UserContext'
 
+const findChar = (name) => {
+    let result;
+    for (let i = 0; i < state.characters.length; i++) {
+        if (state.characters[i].name === name) {
+            result = i;
+        }
+    }
+    if (!result) {
+        return "Character not found"
+    }
+}
+
+const findPath = () => {
+
+}
+
 const UserReducer = (state, action) => {
     switch (action.type) {
         case 'INITIAL_SET':
             console.log('loading')
             localStorage.setItem("test", "save")  
+
             return {
                 ...state,
             }
         case 'ADD_CHARACTER':
-            console.log(state)
-            console.log(action.payload)
+            const { name, description, color } = action.payload
+            let newChar = {
+                name: name,
+                description: description,
+                color: color,
+                paths: [],
+            }
+            state.characters.push(newChar)
+
             return {
                 ...state,
             }
         case 'DELETE_CHARACTER':
+            const { name } = action.payload
+
+            // Find the right character and delete it
+
             return {
                 ...state,
             }
         case 'EDIT_CHARACTER':
+            const { name, description, color } = action.payload
+
+            // Find the right path and save it
+
+            let editChar = {
+                name: name ? name : oldName,
+                description: description ? description : oldDescription,
+                color: color ? color : oldColor,
+                paths: oldPaths,
+            }
+
+            // replace editChar with existing character
+
             return {
                 ...state,
             }
         case 'ADD_PATH':
+            const { character, name, description, start, end } = action.payload
+
+            let newPath = {
+                name: name,
+                description: description,
+                start: start,
+                end: end,
+            }
+
+            //state.characters.push(newPath)
+
             return {
                 ...state,
             }
         case 'DELETE_PATH':
+            const { character, name } = action.payload
+
+            // Find the right path and delete it
+
             return {
                 ...state,
             }
         case 'EDIT_PATH':
+            const { character, name, description, start, end } = action.payload
+
+            // Find the right path and save it
+
+            let editPath = {
+                name: name ? name : oldName,
+                description: description ? description : oldDescription,
+                start: start ? start : newStart,
+                end: end ? end : newEnd,
+            }
+
+            // replace editPath with the old path
+
             return {
                 ...state,
             }
