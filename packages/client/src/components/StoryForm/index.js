6bebc6856ec01b20f68b7ed93f2eb00638db8127
anthony klein <netflixsucks179@gmail.com>
2021-09-28 10:33:22 -0700

    storyform 2

--- packages/client/src/components/StoryForm/index.js
+++ packages/client/src/components/StoryForm/index.js
@@ -1,31 +1,9 @@
 import { render } from '@testing-library/react';
-import React from 'react'
+import React, {useState } from 'react'
 import './StoryForm.css'
 
 export default function StoryForm() {
-    constructor(props) {
-    super(props);
-    this.state = {value:''}
-    this.handleChamge = this.handleChange.bind(this);
-    this.handleSubmit.bind(this);
-    }
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
+   const [username, setUserName] = useState('')
+   const [description, setDescription] = useState('')
 
-    handleSubmit(event) {
-        alert('a name was submitted:' +this.state.value);
-        event.preventDefault();
-    }
-    render(){
-        return (
-            <form onSubmit= {this.handleSubmit}>
-            <label>
-                Name:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-            </form>
-        )
-    }
-    
+}
