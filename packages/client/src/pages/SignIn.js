54048f98846b1681b572ad677717ca53e57094fa
Kysif <78167483+Kysif@users.noreply.github.com>
2021-09-28 12:44:45 -0500

    Update SignIn.js

--- packages/client/src/pages/SignIn.js
+++ packages/client/src/pages/SignIn.js
@@ -1,24 +1,11 @@
-import React from "react";
-import SignInLogic from "components/Signin";
+import React from 'react'
+import SignInLogic from 'components/Signin'
 export default function SignIn() {
   return (
     <>
-<<<<<<< HEAD
-    <div>
-       <SignInLogic>
-         
-       </SignInLogic>
-    </div>
-    <div>
-      sighn up page here!
-      <button id ='signup'>
-      </button>
-    </div>
-=======
       <div>
         <SignInLogic></SignInLogic>
       </div>
->>>>>>> 8657f3c5f8c201e2a301e01ae6b541f76aa1f623
     </>
-  );
+  )
 }
