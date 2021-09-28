c362dc0be19adc71db0c19936c5c378ee5be7823
anthony klein <netflixsucks179@gmail.com>
2021-09-28 09:51:06 -0700

    storyform

--- packages/client/src/components/StoryForm/index.js
+++ packages/client/src/components/StoryForm/index.js
@@ -1,13 +1,31 @@
+import { render } from '@testing-library/react';
 import React from 'react'
 import './StoryForm.css'
 
 export default function StoryForm() {
-    return (
-        <div>
-            <h1>Create a new Story</h1>
-            <div className="stories">
-              <div className="story"></div>
-            </div>
-        </div>
-    )
-}
+    constructor(props) {
+    super(props);
+    this.state = {value:''}
+    this.handleChamge = this.handleChange.bind(this);
+    this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('a name was submitted:' +this.state.value);
+        event.preventDefault();
+    }
+    render(){
+        return (
+            <form onSubmit= {this.handleSubmit}>
+            <label>
+                Name:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+    
