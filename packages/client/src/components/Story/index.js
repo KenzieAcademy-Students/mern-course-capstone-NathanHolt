978d05a99eeedd3972e2315871c66c01311c169e
JMEN98 <78163678+JMEN98@users.noreply.github.com>
2021-09-30 11:13:46 -0400

    useihistory

--- packages/client/src/components/Story/index.js
+++ packages/client/src/components/Story/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 import './Story.css'
 
-export default function Story() {
+export default function Story(props) {
+
+  let history = useHistory();
+
+  function handleClick() {
+    history.push(`/story/${props.name} `);
+  }
+
+
     return (
         <div>
-            <div className="story"></div>
+            <div onClick={handleClick} className="story"
+
+            >
+              <h2>{props.name}</h2>
+             <p>{props.description}</p>
+            </div>
+
+
+            
+      Go to story 
+    
         </div>
     )
 }
