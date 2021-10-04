435556d5cb22634f35a11c2dcfd57a218ddb68a3
Nathan Holt <nateofakin@gmail.com>
2021-10-04 13:31:18 -0400

    Fixed a big bug with signUp

--- packages/client/src/pages/SignUp.js
+++ packages/client/src/pages/SignUp.js
@@ -23,7 +23,7 @@ export default function SignUp() {
       password: password,
       email: email,
     })
-    localStorage.setItem("user", username)
+    // localStorage.setItem("user", username)
     history.push(`/user/${username}`)
   }
   return (
